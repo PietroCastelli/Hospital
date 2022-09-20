dba78582781f2d63405fa1f7541be296055d6bf0
PietroCastelli <castelli.p@stesi.eu>
2022-09-20 18:18:39 +0200

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Hospital",
+
+    'summary': """Description""",
+
+    'description': """
+        
+    """,
+
+    'author': "STeSI",
+    'website': "https://www.stesi.eu",
+
+
+    'category': 'Uncategorized',
+    'version': '14.0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+    'application':True,
+
+    # always loaded
+    'data': [
+        #'security/ir.model.access.csv',
+        'views/res_partner.xml'
+
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+
+    ],
+}
