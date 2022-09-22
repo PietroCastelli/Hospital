363dbbb0bdbd75f93d58fb7b3b34280afffd0685
PietroCastelli <castelli.p@stesi.eu>
2022-09-22 11:08:30 +0200

    fix hospitalization , fix

--- models/hospitalization.py
+++ models/hospitalization.py
@@ -5,10 +5,11 @@ class Hospitalization(models.Model):
     _name = 'hospital.hospitalization'
     _description = 'hospital.hospitalization'
     name = fields.Char(string="name")
+    note = fields.Html(string="testo")
     paziente_id = fields.Many2one("res.partner", domain="[('isPatient', '=',True)]")
     doctor_id = fields.Many2one("res.partner",domain="[('isDoctor', '=',True)]")
     date_hospitalization_in = fields.Datetime(string="Check in")
     date_hospitalization_out = fields.Datetime(string="Check out")
-    disease = fields.Char(string="disease")
+    disease = fields.Char(string="Disease")
     room_id = fields.Many2one("hospital.room")
 
